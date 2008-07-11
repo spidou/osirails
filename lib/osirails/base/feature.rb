91f8ff0e39c24b726090ba03269c1fcb25fed61d
Ulrich TRAJEAN <Ulrich.TRAJEAN@supinfo.com>
2008-07-11 06:50:56 +0000

    ulrich

--- lib/osirails/base/feature.rb
+++ lib/osirails/base/feature.rb
@@ -6,9 +6,58 @@
 module Osirails
   module Base
     class Feature
+      @@name = ""
+      @@version = ""
+      @@dependencies = []
+      @@conflicts = []
+      @@installed = false
+      @@actived = false
+
       def initialize
-         
       end
+      
+      def installed?        
+      end
+      
+      def activated?
+      end
+      
+      def has_dependecies?  
+      end
+      
+      def is_dependence?
+      end
+      
+      def has_conflicts?
+      end
+      
+      def installable?
+      end
+      
+      def able_to_activate?
+      end
+      
+      def able_to_deactivate?
+      end
+      
+      def enable
+      end
+      
+      def disable
+      end
+      
+      def install
+      end
+      
+      def uninstall
+      end
+      
+      def remove
+      end
+      
+      def check
+      end
+      
     end
   end
 end
