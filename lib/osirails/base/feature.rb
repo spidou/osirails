e54c7b1a48315ca269fb6ff68667de4e93b4c035
Ulrich TRAJEAN <Ulrich.TRAJEAN@supinfo.com>
2008-07-11 06:32:27 +0000

    ulrich

---
+++ lib/osirails/base/feature.rb
@@ -0,0 +1,14 @@
+# 
+# To change this template, choose Tools | Templates
+# and open the template in the editor.
+ 
+
+module Osirails
+  module Base
+    class Feature
+      def initialize
+         
+      end
+    end
+  end
+end
