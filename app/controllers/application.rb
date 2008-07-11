eb3c572db73073a02222a57b0136a87f6a42cdd4
Ulrich TRAJEAN <Ulrich.TRAJEAN@supinfo.com>
2008-07-11 09:42:32 +0000

    ulrich

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -12,6 +12,5 @@ class ApplicationController < ActionController::Base
   # Uncomment this to filter the contents of submitted sensitive data parameters
   # from your application log (in this case, all fields with names like "password"). 
   # filter_parameter_logging :password
-  
-  Test.
+puts "Application Controller"
 end
