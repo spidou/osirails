fb52ae53073d0a3cbcb195420b3de83111379092
Benjamin LAN-SUN-LUK <Benjamin.LAN-SUN-LUK@supinfo.com>
2008-07-16 10:21:31 +0000

    Modification de la fonction installable?

--- app/models/feature.rb
+++ app/models/feature.rb
@@ -1,9 +1,9 @@
 class Feature < ActiveRecord::Base
   serialize :dependencies
   serialize :conflicts
-  
-  @missing_dependencies = []
-  @feature_conflicts = []
+
+  # Accessors  
+  attr_reader :feature_conflicts, :missing_dependencies
   
   def installed?
     self.installed
@@ -14,11 +14,7 @@ class Feature < ActiveRecord::Base
   end
   
   def has_dependencies?
-    if self.dependencies != nil
-      self.dependencies.size > 0
-    else
-      false
-    end
+    self.dependencies != nil and self.dependencies.size > 0 ? true : false
   end
 
   def child_dependencies
@@ -40,28 +36,46 @@ class Feature < ActiveRecord::Base
   end
   
   def has_conflicts?
-    if self.conflicts != nil
-        self.conflicts.size > 0
-    else
-        false
-    end
+    self.conflicts != nil and self.conflicts.size > 0 ? true : false
   end
-  
+
   def installable?
+    # Test if the current feature has conflicts or dependencies with other feature
     return true if !has_dependencies? and !has_conflicts?
-    @missing_dependencies ||= []
-    if has_dependencies?
-      dependencies.each do |dep|
-        if Feature.find(:all, :conditions =>["name=? and version in (?) ",dep[:name], dep[:version]]).size == 0
-           @missing_dependencies << dep
+    
+    # Test if the current feature has a conflict with an installed feature
+    @feature_conflicts = []
+    if has_conflicts?
+      conflicts.each do |conflict|
+        if Feature.find(:all, :conditions => ["name=? and version in (?) and installed = 1", conflict[:name], conflict[:version]]).size > 0
+           @feature_conflicts << conflict
         end     
       end
     end
-    if @missing_dependencies.size > 0
-      false
-    else
-      true
+    
+    # Test if the current feature is not present in the conflicts list of all other features
+    Feature.find(:all, :conditions => ["installed = 1"]).each do |feature|
+      if feature.has_conflicts?
+        feature.conflicts.each do |conflict|
+          if conflict[:name] == self.name and conflict[:version].include?(self.version)
+            @feature_conflicts << conflict
+          end
+        end
+      end
     end
+    
+    # Test if the current feature's dependencies are installed
+    @missing_dependencies = []
+    if has_dependencies?
+      dependencies.each do |dependence|
+        if Feature.find(:all, :conditions => ["name=? and version in (?) and installed = 1", dependence[:name], dependence[:version]]).size == 0
+           @missing_dependencies << dependence
+        end     
+      end
+    end
+    @feature_conflicts = @feature_conflicts.uniq
+    @missing_dependencies = @missing_dependencies.uniq
+    @missing_dependencies.size > 0 || @feature_conflicts.size > 0 ? false : true
   end
   
   def able_to_activate?
