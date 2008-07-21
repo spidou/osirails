6f15d16de667f992ec511ad6f249c26ed3992bac
Benjamin LAN-SUN-LUK <Benjamin.LAN-SUN-LUK@supinfo.com>
2008-07-21 08:04:27 +0000

    Suppression de business_object

--- lib/osirails/business_object.rb
+++
@@ -1,8 +0,0 @@
-module Osirails
-  class BusinessObject < ActiveRecord::Base
-    include Permissible 
-    def self.can_list?(roles = [])
-      Permission.can_list?(Osirails::Document, roles)
-    end
-  end
-end
