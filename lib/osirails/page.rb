fb1707f2f36eed73d60d01d69c509b2042ae2350
Ulrich TRAJEAN <Ulrich.TRAJEAN@supinfo.com>
2008-07-22 13:11:57 +0000

    ulrich

--- lib/osirails/page.rb
+++ lib/osirails/page.rb
@@ -2,39 +2,57 @@ module Osirails
   class Page < ActiveRecord::Base
     include Permissible
     
-    include ActiveRecord::Acts::List
-    include ActiveRecord::Acts::Tree
-    
-    acts_as_tree :order =>:position, :counter_cache => :pages_count
+    acts_as_tree :order =>:position
     acts_as_list :scope => :parent_id
     
     belongs_to :parent_page, :class_name =>"Page", :foreign_key => "parent_id"
     
-    def add_list_item()
-      
+    # This method add a new page
+    # parent : represent the future parent_page
+    # new_page : is a hash that content the new page properties
+    def Page.add_list_item(parent = nil, new_page = {})
+      child = Page.new(:title_link => new_page[:title_link], :description_link => new_page[:description_link], :name => new_page[:name])
+      unless parent.nil?
+        parent.children << child
+      end
+      child.save
     end
 
-    # This methods permit to change the parent of a item group 
-    # new_parent : represent the new parent
-    # pages : represent the pages collection for the parent changing    
+    # This method permit to change the parent of a item
+    # new_parent : represent the new parent            
+    def move(new_parent)
+       return false if new_parent.id == self.id
+       return false if new_parent.parent_id == self.id
+       return false if new_parent.ancestors.include?(self)
+       
+       self.parent_id = new_parent.id
+       self.position = new_parent.children.size + 1
+       self.save         
+     end       
     
-    def Page.move(new_parent, pages = [])
-      Page.transaction do
-        pages.each do |page|
-          p = Page.new(:title_link => page.title_link, :description_link => page.description_link, :url => page.url , :name => page.name)
-          p.parent_id = new_parent.id
-          p.save
-          page.destroy
-        end
+    def delete_item
+      if base_item?(self)
+        puts "This Page is a Basic page"
+        return false
       end
-    end  
-      
-    def rename(page = {})
-      
+      if has_children?(self)
+        puts "This page have children. Please move them in a different page or remove them before delete the page "
+        return false
+      end
+      self.destroy
+      return true
     end
     
-    def delete_item(pages_id = [])
-      
+    # This method test if the page is a Basic item
+    def base_item?(page)
+      return false if page.base
     end
+    
+    # This method test if the page has got children
+    def has_children?(page)
+      page.children.empty? ? true : false
+    end
+    
   end
 end
+# Rajouter le champ type
