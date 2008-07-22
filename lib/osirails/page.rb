79243a7dd71d4ef508e677037b42556055302879
Ulrich TRAJEAN <Ulrich.TRAJEAN@supinfo.com>
2008-07-22 07:47:44 +0000

    ulrich

--- lib/osirails/page.rb
+++ lib/osirails/page.rb
@@ -5,16 +5,28 @@ module Osirails
     include ActiveRecord::Acts::List
     include ActiveRecord::Acts::Tree
     
-    acts_as_tree :order =>:position
+    acts_as_tree :order =>:position, :counter_cache => true
     acts_as_list :scope => :parent_id
+    
+    belongs_to :parent_page, :class_name =>"Page", :foreign_key => "parent_id", :counter_cache => :pages_count
     
     def add_list_item(page = {})
       
     end
 
-    def move(items = [])
+    # This methods permit to change the parent of a item group 
+    # new_parent_id : represent the new parent_id
+    # pages_id : represent the pages collection for the parent changing    
+    def Page.move(new_parent_id, pages_id = [])
       # Ne pas oublier de changer également les enfant de l'objet que l'on veut dépalcer
-      
+      pages_id.each do |page|
+        child = Page.find_by_id(page)
+        parent_page = Page.find_by_id(new_parent_id)
+        parent_page.children << child
+        child.move_to_bottom
+        parent_page.save
+        child.save                
+      end
     end  
     
     def rename(page = {})
