167a5d7153a2a7cd5b11d8b2147574b817d29554
Benjamin LAN-SUN-LUK <Benjamin.LAN-SUN-LUK@supinfo.com>
2008-07-22 06:47:09 +0000

    Modification de environnement.rb

--- config/environment.rb
+++ config/environment.rb
@@ -34,7 +34,7 @@ Rails::Initializer.run do |config|
   # Add additional load paths for your own custom dirs
   # config.load_paths += %W( #{RAILS_ROOT}/extras )
   config.load_paths += %W( #{RAILS_ROOT}/lib/osirails/ )
-  config.plugin_paths = ["#{RAILS_ROOT}/lib/features", "#{RAILS_ROOT}/vendor/features"]
+  config.plugin_paths += ["#{RAILS_ROOT}/lib/features", "#{RAILS_ROOT}/vendor/features"]
 
   # Force all environments to use the same logger level
   # (by default production uses :info, the others :debug)
