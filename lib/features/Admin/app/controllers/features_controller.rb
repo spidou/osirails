8934cd874f28374af429e605653e9b2cdd860cd9
Julien MORILLE <gun974@gmail.com>
2008-07-22 12:57:32 +0000

    modif Feature admin

--- lib/features/Admin/app/controllers/features_controller.rb
+++ lib/features/Admin/app/controllers/features_controller.rb
@@ -1,5 +1,6 @@
 class FeaturesController < ApplicationController
 
+  #Method to collect features and to show them into the index view
   def index
     @features = Osirails::Feature.find(:all)
 
@@ -9,22 +10,47 @@ class FeaturesController < ApplicationController
     end
   end
 
+  # Method to show up messages concerning features enabling
+  #flash[:notice] shows method's succes message
+  #flash[:error] shows method's failure message with some details concerning dependances  
   def enable
     @feature = Osirails::Feature.find(params[:id])
-    @feature.enable
-    # TODO Faire le même affichage des flashs que ceux en bas. Manque: features_not_activated
-    flash[:notice] = @feature.name + " est activé."
+   if  @feature.enable
+     flash[:notice] = @feature.name + " est activé."
+   else  
+     flash[:error] = "Erreur(s) lors de l'activation de " + @feature.name
+     unless @feature.features_not_activated.empty?
+       flash[:error] << "<br /><br />Dépendance(s) non activés requise(s): "
+       @feature.features_not_activated.each do |error|
+          flash[:error] << "<br />" + error[:name] + " [ v:" + error[:version] +  "]"
+       end
+     end
+   end
     redirect_to features_path
   end
 
+ # Method to show up messages concerning features disabling
+ #flash[:notice] shows method's succes message
+ #flash[:error] shows method's failure message with some details concerning modules depending current module
   def disable
     @feature = Osirails::Feature.find(params[:id])
-    @feature.disable
-    # TODO Pareil pour: children_activated
-    flash[:notice] = @feature.name + " est désactivé."
+     if @feature.disable
+      flash[:notice] = @feature.name + " est désactivé."
+     else
+      flash[:error] = "Erreur(s) lors de la désactivation de " + @feature.name
+        unless @feature.children_activated.empty?
+          flash[:error] << "<br /><br />D'autres modules dépendent de ce module: "
+           @feature.children_activated.each do |error|
+              flash[:error] << "<br />" + error[:name] + " [ v:" + error[:version] + "]"
+           end
+        end
+     end
     redirect_to features_path
   end
   
+  # Method to show up messages concerning features installing
+  #flash[:notice] shows method's succes message
+  #flash[:error] shows method's failure message with some details concerning dependance and conflicting modules
   def install
     @feature = Osirails::Feature.find(params[:id])
     if @feature.install
@@ -32,15 +58,15 @@ class FeaturesController < ApplicationController
     else
       flash[:error] = "Erreur(s) lors de l'installation de " + @feature.name
       unless @feature.missing_dependencies.empty?
-        flash[:error] << "<br />Dépendance(s) requise(s): "
+        flash[:error] << "<br /><br />Dépendance(s) non installés requise(s): "
         @feature.missing_dependencies.each do |error|
-          flash[:error] << "<br />" + error[:name] + " [" + error[:version].join(" | ") + "]"
+          flash[:error] << "<br />" + error[:name] + " [ v:" + error[:version].join(" | v: ") + "]"
         end
       end
       unless @feature.feature_conflicts.empty?
         flash[:error] << "<br />Conflit(s) détecté(s): "
         @feature.feature_conflicts.each do |error|
-          flash[:error] << "<br />" + error[:name] + " [" + error[:version].join(" | ") + "]"
+          flash[:error] << "<br />" + error[:name] + " [ v:" + error[:version].join(" | v: ") + "]"
         end
       end
       unless @feature.installation_messages.empty?
@@ -51,7 +77,10 @@ class FeaturesController < ApplicationController
     end
     redirect_to features_path
   end
-
+  
+  # Method to show up messages concerning features uninstalling
+  #flash[:notice] shows method's succes message
+  #flash[:error] shows method's failure message with some details concerning modules depending current module
   def uninstall
     @feature = Osirails::Feature.find(params[:id])
     if @feature.uninstall
@@ -59,9 +88,9 @@ class FeaturesController < ApplicationController
     else
       flash[:error] = "Erreur(s) lors de la désinstallation de " + @feature.name
       unless @feature.features_uninstallable.empty?
-        flash[:error] << "<br />D'autres modules dépendent de ce module: "
+        flash[:error] << "<br /><br />D'autres modules dépendent de ce module: "
         @feature.features_uninstallable.each do |error|
-          flash[:error] << "<br />" + error[:name] + " Versions: " + error[:version]
+          flash[:error] << "<br />" + error[:name] + " [v: " + error[:version]+"]"
         end
       end
       unless @feature.uninstallation_messages.empty?
@@ -69,8 +98,16 @@ class FeaturesController < ApplicationController
           flash[:error] << "<br />" + error
         end
       end      
-
     end
-    redirect_to features_path
+      redirect_to  :action => "show"
+  end
+  
+    def show
+    @feature = Osirails::Feature.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.xml  { render :xml => @feature }
+    end
   end
 end
