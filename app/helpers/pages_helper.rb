003f7a13ec4ad3e70a8912befaf781a0a120b75a
Ulrich TRAJEAN <Ulrich.TRAJEAN@supinfo.com>
2008-07-23 06:11:10 +0000

    ulrich

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,15 +1,22 @@
 module PagesHelper
+  
+  #This method return an array with all pages
   def get_pages_array
+    pages = Osirails::Page.find_all_by_parent_id(nil)
+    parent_array = []
     
-    def boucles
+    # This method insert in the $parent_array the page
+    def insert_page(pages,parent_array)
       pages.each do |page|
-      @@parent_array << page
+      parent_array << page
+          
+          #If the page has children, the insert_page method is call.
           if page.children.size > 0
-            Page.get_structure(page.children)
+            insert_page(page.children,parent_array)
           end
         end
     end
-            
-          
+    insert_page(pages,parent_array)
+    parent_array
   end
 end
