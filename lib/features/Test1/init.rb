c8262f0db517c0c09f4834fdaf179467f6ee953f
Ulrich TRAJEAN <Ulrich.TRAJEAN@supinfo.com>
2008-07-24 07:04:04 +0000

    ulrich

--- lib/features/Test1/init.rb
+++ lib/features/Test1/init.rb
@@ -1,4 +1,4 @@
 require 'yaml'
 yaml = YAML.load(File.open(directory+'/config.yml'))
 require File.join(directory, '../initialize.rb')
-init(yaml, config)
+init(yaml, config, directory)
--- lib/features/initialize.rb
+++ lib/features/initialize.rb
@@ -1,13 +1,13 @@
-def init(yaml, config)
+def init(yaml, config, path)
   
   # Load every file in the app directory
-  controller_path = File.join(directory, 'app', 'controllers')
+  controller_path = File.join(path, 'app', 'controllers')
   $LOAD_PATH << controller_path
   Dependencies.load_paths << controller_path
   config.controller_paths << controller_path
-  ActionController::Base.append_view_path(File.dirname(__FILE__) + '/app/views/')
-  $LOAD_PATH << File.join(directory, 'app', 'helpers')
-  $LOAD_PATH << File.join(directory, 'app', 'models')
+  ActionController::Base.append_view_path(path + '/app/views/')
+  $LOAD_PATH << File.join(path, 'app', 'helpers')
+  $LOAD_PATH << File.join(path, 'app', 'models')
 
   # This variable store the configuration of features from config.yml file 
   name = yaml['name']
