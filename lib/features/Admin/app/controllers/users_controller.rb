a82a83327532952c1da33944ed6c46740c0f4099
Benjamin LAN-SUN-LUK <Benjamin.LAN-SUN-LUK@supinfo.com>
2008-07-28 14:04:04 +0000

    Finition de users

--- lib/features/Admin/app/controllers/users_controller.rb
+++ lib/features/Admin/app/controllers/users_controller.rb
@@ -54,32 +54,36 @@ class UsersController < ApplicationController
     end
   end
 
-#PUT /users/1
-#PUT /users/1.xml
- def update
-   params[:user][:role_ids] ||= []
-   @user = User.find(params[:id])
-   respond_to do |format|
-     if @user.update_attributes(params[:user])
-       flash[:notice] = 'User was successfully updated.'
-       format.html { redirect_to(@user) }
-       format.xml  { head :ok }
-     else
-       format.html { render :action => "edit" }
-       format.xml  { render :xml => @user.errors, :status => :unprocessable_entity }
-     end
-   end
- end
-
-# DELETE /users/1
-# DELETE /users/1.xml
-def destroy
-  @user = User.find(params[:id])
-  @user.destroy
-
-  respond_to do |format|
-    format.html { redirect_to(users_url) }
-    format.xml  { head :ok }
+  #PUT /users/1
+  #PUT /users/1.xml
+  def update
+    params[:user][:role_ids] ||= []
+    @user = User.find(params[:id])
+    if params[:user][:password].empty?
+      params[:user].delete(:password)
+      params[:user].delete(:password_confirmation)
+    end
+    respond_to do |format|
+      if @user.update_attributes(params[:user])
+        flash[:notice] = 'User was successfully updated.'
+        format.html { redirect_to(@user) }
+        format.xml  { head :ok }
+      else
+        format.html { render :action => "edit" }
+        format.xml  { render :xml => @user.errors, :status => :unprocessable_entity }
+      end
+    end
   end
-end
+
+  # DELETE /users/1
+  # DELETE /users/1.xml
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+
+    respond_to do |format|
+      format.html { redirect_to(users_url) }
+      format.xml  { head :ok }
+    end
+  end
 end
--- lib/features/Admin/app/models/user.rb
+++ lib/features/Admin/app/models/user.rb
@@ -1,5 +1,21 @@
 class User < ActiveRecord::Base
   has_and_belongs_to_many :roles
-  validates_uniqueness_of :username
-  validates_confirmation_of :password
+  # Validates
+  validates_uniqueness_of :username, :message => "existe déjà"
+  validates_presence_of :username, :password, :message => "ne peut être vide"
+  validates_confirmation_of :password, :message => "ne correspondent pas≈"
+  before_save :password_encryption
+  # Requires
+  require "digest/sha1"
+  
+  # Method to encrypt the password to the data base
+  def password_encryption
+    self.password = Digest::SHA1.hexdigest(self.password)
+  end
+  
+  # Method to verify if the password pass by argument is the same as the password in the database
+  def compare_password(new_password)
+    self.password == Digest::SHA1.hexdigest(new_password)
+  end
+
 end
