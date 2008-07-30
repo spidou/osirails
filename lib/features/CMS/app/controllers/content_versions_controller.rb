3db2606fda32b19cc7425ddba22a508d97fdb598
Ulrich TRAJEAN <Ulrich.TRAJEAN@supinfo.com>
2008-07-30 12:16:50 +0000

    ulrich

--- lib/features/CMS/app/controllers/content_versions_controller.rb
+++ lib/features/CMS/app/controllers/content_versions_controller.rb
@@ -7,10 +7,8 @@ class ContentVersionsController < ApplicationController
     @content_version = ContentVersion.find(params[:id])
     # Static_page content the original static_page
     @content = Content.find(@content_version.content.id)
-    @author = User.find(@content_version.author, :include => [:employee])
+    @author = User.find(@content.author, :include => [:employee])
     # TODO : mettre ce code dans le model
-    @contributors = User.find_all_by_id(@content_version.contributors, :include => [:employee])
-    @contributors_full_names = []
-    @contributors.each {|contributor| @contributors_full_names << contributor.employee.fullname}
+    @contributor = User.find(@content_version.contributors, :include => [:employee]) unless @content_version.contributors.nil?
   end
 end
