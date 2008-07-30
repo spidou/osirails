96a7bce531f807c8bb8bf6873f0166e39fa155f3
Benjamin LAN-SUN-LUK <Benjamin.LAN-SUN-LUK@supinfo.com>
2008-07-30 11:13:31 +0000

    Petite amélioration

--- lib/features/Admin/app/controllers/business_object_permissions_controller.rb
+++ lib/features/Admin/app/controllers/business_object_permissions_controller.rb
@@ -9,12 +9,12 @@ class BusinessObjectPermissionsController < ApplicationController
 
   def update
     transaction_error = BusinessObjectPermission.transaction do
-      BusinessObjectPermission.update_all("`list` = 0, `view` = 0, `add` = 0, `edit` = 0, `delete` = 0", :has_permission_type => params[:name])
-      BusinessObjectPermission.update_all("`list` = 1", :role_id => params[:list], :has_permission_type => params[:name])
-      BusinessObjectPermission.update_all("`view` = 1", :role_id => params[:view], :has_permission_type => params[:name])
-      BusinessObjectPermission.update_all("`add` = 1", :role_id => params[:add], :has_permission_type => params[:name])
-      BusinessObjectPermission.update_all("`edit` = 1", :role_id => params[:edit], :has_permission_type => params[:name])
-      BusinessObjectPermission.update_all("`delete` = 1", :role_id => params[:delete], :has_permission_type => params[:name])
+      BusinessObjectPermission.update_all("`list` = 0, `view` = 0, `add` = 0, `edit` = 0, `delete` = 0", :has_permission_type => params[:id])
+      BusinessObjectPermission.update_all("`list` = 1", :role_id => params[:list], :has_permission_type => params[:id])
+      BusinessObjectPermission.update_all("`view` = 1", :role_id => params[:view], :has_permission_type => params[:id])
+      BusinessObjectPermission.update_all("`add` = 1", :role_id => params[:add], :has_permission_type => params[:id])
+      BusinessObjectPermission.update_all("`edit` = 1", :role_id => params[:edit], :has_permission_type => params[:id])
+      BusinessObjectPermission.update_all("`delete` = 1", :role_id => params[:delete], :has_permission_type => params[:id])
     end
     if transaction_error
       flash[:notice] = "Les permissions ont été modifié avec succés"
