ba50d28baec62cb470abd7540bd630b3ce2cbf87
Benjamin LAN-SUN-LUK <Benjamin.LAN-SUN-LUK@supinfo.com>
2008-07-30 04:29:28 +0000

    Petite optimisation du fichier

--- lib/features/Admin/app/controllers/business_object_permissions_controller.rb
+++ lib/features/Admin/app/controllers/business_object_permissions_controller.rb
@@ -9,29 +9,17 @@ class BusinessObjectPermissionsController < ApplicationController
   end
 
   def update
-    @business_object_permissions = BusinessObjectPermission.find(:all, :conditions => ["has_permission_type = ?", params[:name]] )
-    save_error = false
-    @business_object_permissions.each do |business_object_permission|
-      params[:list] ||= []
-      business_object_permission.list = params[:list].include?(business_object_permission.role_id.to_s)
-      params[:view] ||= []
-      business_object_permission.view = params[:view].include?(business_object_permission.role_id.to_s)
-      params[:add] ||= []
-      business_object_permission.add = params[:add].include?(business_object_permission.role_id.to_s)
-      params[:edit] ||= []
-      business_object_permission.edit = params[:edit].include?(business_object_permission.role_id.to_s)
-      params[:delete] ||= []
-      business_object_permission.delete = params[:delete].include?(business_object_permission.role_id.to_s)
-      save_error ||= !business_object_permission.save
-    end
-
-    unless save_error
+    if (BusinessObjectPermission.update_all("`list` = 0, `view` = 0, `add` = 0, `edit` = 0, `delete` = 0", :has_permission_type => params[:name]) &&
+        BusinessObjectPermission.update_all("`list` = 1", :role_id => params[:list], :has_permission_type => params[:name]) &&
+        BusinessObjectPermission.update_all("`view` = 1", :role_id => params[:view], :has_permission_type => params[:name]) &&
+        BusinessObjectPermission.update_all("`add` = 1", :role_id => params[:add], :has_permission_type => params[:name]) &&
+        BusinessObjectPermission.update_all("`edit` = 1", :role_id => params[:edit], :has_permission_type => params[:name]) &&
+        BusinessObjectPermission.update_all("`delete` = 1", :role_id => params[:delete], :has_permission_type => params[:name]) )
       flash[:notice] = "Les permissions ont été modifié avec succés"
       redirect_to(:action => 'edit', :name => params[:name]) 
     else
       flash[:error] = "Erreur lors de la mise à jour des permissions"
-      render :action => "edit" 
+      redirect_to :action => "edit"
     end
   end
-
 end
