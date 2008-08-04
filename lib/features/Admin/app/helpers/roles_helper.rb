5c4ab53f9745fa1f74e3be9a57975b30856af444
Julien MORILLE <gun974@gmail.com>
2008-08-04 12:55:14 +0000

    feature admin password policies

--- lib/features/Admin/app/helpers/roles_helper.rb
+++ lib/features/Admin/app/helpers/roles_helper.rb
@@ -2,7 +2,7 @@ module RolesHelper
   
   # to get a preview  of the string
   def preview(string,length)
-    string.length>length ? string[0,length] +" (...)" : string 
+    string.length>length and !string.nil? ? string[0,length] +" (...)" : string 
   end
   
 end
