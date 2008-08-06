a5e8119a423ec8a440ad929d9284ddd0fdf4d85e
Jeremie LAVAL <Jeremie.LAVAL@supinfo.com>
2008-08-06 10:23:56 +0000

    Configuration_Manager AVEC FIXME

--- lib/osirails/configuration_manager.rb
+++ lib/osirails/configuration_manager.rb
@@ -19,7 +19,7 @@ class ConfigurationManager
     end
   
     def ConfigurationManager.find_configurations_for(feature_name, controller_name)
-      ConfigurationManager.methods.sort.grep(/^#{feature_name.downcase}_#{controller_name}.*/)
+      ConfigurationManager.methods.sort.grep(/^#{feature_name.downcase}_#{controller_name}.*[^=c]$/) #FIXME Faire en sorte que ça gère _desc
     end
   
 end
