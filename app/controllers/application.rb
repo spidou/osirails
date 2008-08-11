461c6f0c1f964fa3355d7edc315d7c969c9e220b
Ulrich TRAJEAN <Ulrich.TRAJEAN@supinfo.com>
2008-08-11 12:19:22 +0000

    ulrich

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -25,19 +25,6 @@ class ApplicationController < ActionController::Base
 
   #initialize dynmamic methods from the configurations' table of the database
   ConfigurationManager.initialize_options
-  
-    def my_text_field_with_auto_complete(object, method, tag_options = {}, completion_options = {})
-    if(tag_options[:index])
-      tag_name = "#{object}_#{tag_options[:index]}_#{method}"
-    else
-      tag_name = "#{object}_#{method}"
-    end
-
-    (completion_options[:skip_style] ? "" : auto_complete_stylesheet) +
-        text_field(object, method, tag_options) +
-        content_tag("div", "", :id => tag_name + "_auto_complete", :class => "auto_complete") +
-        auto_complete_field(tag_name, { :url => { :action => "auto_complete_for_#{object}_#{method}" } }.update(completion_options))
-  end
 
   protected
 
