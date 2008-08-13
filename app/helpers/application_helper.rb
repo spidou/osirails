e80c643cf8de90dc4d358708f39f3b33940c2d94
Mathieu FONTAINE <spidou@gmail.com>
2008-08-13 06:40:39 +0000

    amélioration de la gestion des menus faire "truncate menus; truncate menu_permissions;" avant de relancer le serveur pour bien apprécier les changements

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -21,12 +21,21 @@ module ApplicationHelper
     Menu.find_by_name(controller.controller_name) or raise "The controller '#{controller.controller_name}' should have a menu with the same name"
   end
   
+  def link_to_menu(menu, content = nil)
+    # OPTIMIZE optimize this IF block code
+    if self.respond_to?(menu.name + "_path")
+      link_to(content || menu.title, self.send(menu.name + "_path"))
+    else
+      link_to(content || menu.title, :controller => menu.name)
+    end
+  end
+  
   def display_main_menu
     html = ""
     menu = current_menu
     Menu.mains.each do |m|
       selected = (menu == m or menu.ancestors.include?(m) ? "class=\"selected\"" : "")
-      html << "<li #{selected} title=\"#{m.description}\"><a href=\"\">#{m.title}</a></li>\n"
+      html << "<li #{selected} title=\"#{m.description}\">" + link_to_menu(m) + "</li>\n"
     end
     html
   end
@@ -39,7 +48,8 @@ module ApplicationHelper
     main_menu.children.each do |m|
       first = main_menu.children.first == m ? "id=\"menu_horizontal_first\"": "" #detect if is the first element
       selected = ( ( menu == m or menu.ancestors.include?(m) ) ? "class=\"selected\"" : "") #detect if the element is selected
-      html << "<a href=\"\" title=\"#{m.description}\"><span #{selected} #{first}>#{m.title}</span></a>"
+      #html << "<a href=\"\"><span title=\"#{m.description}\" #{selected} #{first}>#{m.title}</span></a>"
+      html << link_to_menu(m, "<span title=\"#{m.description}\" #{selected} #{first}>#{m.title}</span></a>")
     end
     html.reverse.to_s
   end
