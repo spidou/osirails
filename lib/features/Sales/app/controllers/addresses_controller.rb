a5984a9b736df7c771bab9eaaf881b40aac482e2
Ulrich TRAJEAN <Ulrich.TRAJEAN@supinfo.com>
2008-08-22 11:56:48 +0000

    ulrich

--- lib/features/Sales/app/controllers/addresses_controller.rb
+++ lib/features/Sales/app/controllers/addresses_controller.rb
@@ -1,5 +1,6 @@
 class AddressesController < ApplicationController
   
+  #OPTIMIZE make possible to scroll in the ul div
   def auto_complete_for_country_name
     auto_complete_responder_for_country_name(params[:value])
   end
@@ -9,13 +10,12 @@ class AddressesController < ApplicationController
       :conditions => [ 'LOWER(name) LIKE ?',
         '%' + value.downcase + '%'], 
       :order => 'name ASC',
-      :limit => 8)
+      :limit => 10)
     render :partial => 'addresses/countries'
   end
   
-  #FIXME Change the code to take in consideration the fact that it can have many city with the same name
   def auto_complete_for_city_name
-    country = Country.find_by_name("#{params[:value]}")
+    country = Country.find_by_name("#{params[:country_name]}")
     if country.nil?
       country_id = 0
     else
@@ -30,20 +30,26 @@ class AddressesController < ApplicationController
         :conditions => [ 'LOWER(name) LIKE ? AND country_id = ?',
           '%' + value.downcase + '%', country_id], 
         :order => 'name ASC',
-        :limit => 8)
+        :limit => 10)
+      render :partial => 'addresses/cities_name'
     else 
-      @cities = City.find(:all)
+      @cities = City.find(:all, 
+        :conditions => [ 'LOWER(name) LIKE ?',
+          '%' + value.downcase + '%'], 
+        :order => 'name ASC',
+        :limit => 10)
+      render :partial => 'addresses/cities_name'
     end
-    render :partial => 'addresses/cities_name'
   end
   
   def auto_complete_for_city_zip_code
-   country = Country.find_by_name("#{params[:value]}")
+   country = Country.find_by_name("#{params[:country_name]}")
     if country.nil?
       country_id = 0
     else
       country_id = country.id
     end
+    
       auto_complete_responder_for_zip_code(params[:value], country_id)
   end
   
@@ -53,12 +59,17 @@ class AddressesController < ApplicationController
         :conditions => [ 'zip_code LIKE ? AND country_id = ?',
           '%' + value.to_s + '%', country_id], 
         :order => 'name ASC',
-        :limit => 8)
-      
+        :limit => 10)
+      render :partial => 'addresses/cities_zip_code'
     else
-      @cities = City.find(:all)
+      @cities = City.find(:all,
+        :conditions => [ 'zip_code LIKE ?',
+          '%' + value.to_s + '%'], 
+        :order => 'name ASC',
+        :limit => 10)
+      render :partial => 'addresses/cities_zip_code'
     end
-    render :partial => 'addresses/cities_zip_code'
+    
   end
   
 end
