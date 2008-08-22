7764eed1ad324f4c5c1ac7d0c7bf5833b07ee9c4
Ulrich TRAJEAN <Ulrich.TRAJEAN@supinfo.com>
2008-08-22 14:11:09 +0000

    ulrich

---
+++ db/migrate/20080822135044_modify_thirds.rb
@@ -0,0 +1,21 @@
+class ModifyThirds < ActiveRecord::Migration
+  def self.up
+    remove_column :thirds, :banking_informations
+    remove_column :thirds, :legal_form
+    add_column :thirds, :legal_form_id, :integer
+    add_column :thirds, :iban_id, :integer
+    add_column :thirds, :payment_method_id, :integer
+    add_column :thirds, :payment_time_limit_id, :integer
+  end
+
+  def self.down
+    remove_column :thirds, :legal_form_id
+    remove_column :thirds, :iban_id
+    remove_column :thirds, :payment_method_id
+    remove_column :thirds, :payment_time_limit_id
+    add_column :thirds, :banking_informations, :string
+    add_column :thirds, :legal_form, :string
+    
+  end
+end
+
