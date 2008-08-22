a86afcb7801580dd256814b56af1fcb9c4cf5bd5
Ulrich TRAJEAN <Ulrich.TRAJEAN@supinfo.com>
2008-08-22 05:45:39 +0000

    ulrich

--- lib/features/Sales/app/controllers/activity_sectors.rb.rb
+++ lib/features/Sales/app/controllers/activity_sectors.rb.rb
@@ -1,20 +1,17 @@
 class ActivitySectorsController < ApplicationController
-  
-  auto_complete_for :activity_sectors, :name
-     # Autocomplete
-  def auto_complete_for_activity_sectors_name
-    puts "tes"
-    puts params[:activity_sectors][:name]
-#    auto_complete_responder_for_name(params[:activity_sectors][:name])
+#  
+#  auto_complete_for :activity_sectors, :name
+#     # Autocomplete
+def auto_complete_for_activity_sector_name
+    auto_complete_responder_for_name(params[:activity_sector][:name])
   end
   
   def auto_complete_responder_for_name(value)
-    ActivitySectors.find(:all)
-#    @sectors = ActivitySectors.find(:all, 
-#      :conditions => [ 'LOWER(name) LIKE ?',
-#        '%' + value.downcase + '%'], 
-#      :order => 'name ASC',
-#      :limit => 8)
-#    render :partial => 'addresses/cities_name'
+    @activity_sectors = ActivitySector.find(:all, 
+      :conditions => [ 'LOWER(name) LIKE ?',
+        '%' + value.downcase + '%'], 
+      :order => 'name ASC',
+      :limit => 8)
+    render :partial => 'thirds/activity_sectors'
   end
 end
--- lib/features/Sales/app/controllers/customers_controller.rb
+++ lib/features/Sales/app/controllers/customers_controller.rb
@@ -189,17 +189,4 @@
     @customer.destroy
     redirect_to(customers_path) 
   end
-  
-  def auto_complete_for_activity_sector_name
-    auto_complete_responder_for_name(params[:activity_sector][:name])
-  end
-  
-  def auto_complete_responder_for_name(value)
-    @activity_sectors = ActivitySector.find(:all, 
-      :conditions => [ 'LOWER(name) LIKE ?',
-        '%' + value.downcase + '%'], 
-      :order => 'name ASC',
-      :limit => 8)
-    render :partial => 'thirds/activity_sectors'
-  end
 end
