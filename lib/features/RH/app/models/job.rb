f2cdffd4d5457e4d608bb9e5d6391ad109f28546
Julien MORILLE <gun974@gmail.com>
2008-08-22 05:50:23 +0000

    feature Ressources humaines

--- lib/features/RH/app/models/job.rb
+++ lib/features/RH/app/models/job.rb
@@ -1,3 +1,4 @@
 class Job < ActiveRecord::Base
-  has_and_belongs_to_many :employees, :dependent => :destroy 
-end
+  has_and_belongs_to_many :employees
+  validates_uniqueness_of :name, :message => "la fonction existe déjà!"
+end  
