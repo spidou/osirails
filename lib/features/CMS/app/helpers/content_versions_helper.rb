07e8387ba1d1c798dd20dd4b1954214231d4bf6a
Ulrich TRAJEAN <Ulrich.TRAJEAN@supinfo.com>
2008-08-26 14:08:35 +0000

    ulrich

--- lib/features/CMS/app/helpers/content_versions_helper.rb
+++ lib/features/CMS/app/helpers/content_versions_helper.rb
@@ -2,4 +2,6 @@ module ContentVersionsHelper
   def contributor_full_name(content_version)
     content_version.contributor.employee.last_name + " " + content_version.contributor.employee.first_name
   end
+  
+  
 end
