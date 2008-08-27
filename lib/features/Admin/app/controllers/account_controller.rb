d5efbb42927d101842c76c3ae4425b96a3f35e0c
Benjamin LAN-SUN-LUK <Benjamin.LAN-SUN-LUK@supinfo.com>
2008-08-27 07:10:04 +0000

    Modification du systeme d'account

--- lib/features/Admin/app/controllers/account_controller.rb
+++ lib/features/Admin/app/controllers/account_controller.rb
@@ -1,7 +1,7 @@
 class AccountController < ApplicationController
   skip_before_filter :authenticate
-  before_filter :logged?, :except => [:logout, :expired_password]
-  before_filter :not_logged?, :except => [:login, :lost_password]
+  before_filter :logged?, :only => [:login, :lost_password]
+  before_filter :not_logged?, :only => [:logout, :expired_password]
   layout "login"
 
   # All the following action are accessible without login
