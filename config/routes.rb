10e63f069b3fb22436e16968d8292bb05a71f5a3
Benjamin LAN-SUN-LUK <Benjamin.LAN-SUN-LUK@supinfo.com>
2008-09-01 09:34:06 +0000

    Correction du bug des routes

--- config/routes.rb
+++ config/routes.rb
@@ -67,7 +67,7 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :calendars do |calendar|
     calendar.resources :events
   end
-  map.connect ':controller/:id/:period/:year/:month/:day', :controller => 'calendars', :action => 'show', :period => nil, :year => nil, :month => nil, :day => nil
+  map.connect 'calendars/:id/:period/:year/:month/:day', :controller => 'calendars', :action => 'show', :period => nil, :year => nil, :month => nil, :day => nil
   ### END CALENDAR
 
   ### COMMONS
