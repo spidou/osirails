1417ff405676c8f3e8720a4a65009a1f28be6f02
Ulrich TRAJEAN <Ulrich.TRAJEAN@supinfo.com>
2008-09-01 12:35:29 +0000

    ulrich

--- lib/osirails/document.rb
+++ lib/osirails/document.rb
@@ -1,13 +1,19 @@
-#class Document < ActiveRecord::Base
-#  
-#  @models = []
-#  
-#  def self.add_model(class_name)
-#    @models << class_name
-#  end
-#  
-#  def self.models
-#    @models
-#  end
-#  
-#end
+class Document < ActiveRecord::Base
+  
+  attr_accessor :models
+  
+  @models = []
+  # Add the model name inton models array
+  def self.add_model(model)
+    @models << model
+  end
+  
+  def self.models
+    @models
+  end
+  
+  def self.delete_file(file)
+    
+  end
+  
+end
