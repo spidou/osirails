79ff55dec175d90145d3a31da98f49930aa8ec88
Julien MORILLE <gun974@gmail.com>
2008-09-03 10:42:31 +0000

    lightbox

---
+++ public/javascripts/premium.js
@@ -0,0 +1,12 @@
+function confirm_premium()
+{
+  text = "Etes vous sûr de vouloir ajouter la prime suivante? \n\n"
+  text += "Montant : " + document.getElementById('premium_premium').value + "€ \n";
+  text += "Commentaire : " + document.getElementById('premium_remark').value + "\n";
+  text += "Date : " + document.getElementById('premium_date_3i').value + "/";
+  text += document.getElementById('premium_date_2i').value + "/";
+  text += document.getElementById('premium_date_1i').value + "\n\n";
+  text += "Attention cette action est irreversible !"
+  
+  return confirm(text);
+}
