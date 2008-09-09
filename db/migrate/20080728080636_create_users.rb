b4f2993f7c426d56cd9db86d34a31ff26d860b5c
Julien MORILLE <gun974@gmail.com>
2008-09-09 11:54:06 +0000

    featue RH

--- db/migrate/20080728080636_create_users.rb
+++ db/migrate/20080728080636_create_users.rb
@@ -1,7 +1,7 @@
 class CreateUsers < ActiveRecord::Migration
   def self.up
     create_table :users do |t|
-      t.string :username
+      t.string :username, :unique =>true
       t.string :password
       t.boolean :enabled
       t.datetime :expire_date
