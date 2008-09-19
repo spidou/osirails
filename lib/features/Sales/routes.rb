36ee1043b3fcd694ba6f5af25bd16e4875aa072c
Benjamin LAN-SUN-LUK <Benjamin.LAN-SUN-LUK@supinfo.com>
2008-09-19 05:30:03 +0000

    Routes du module Ventes

--- lib/features/Sales/routes.rb
+++ lib/features/Sales/routes.rb
@@ -1,3 +1,7 @@
 ActionController::Routing::Routes.add_routes do |map|
-#  map.connect 'orders/:id/:controller/:action'
+  map.connect 'orders/:id/commercial/:action', :controller => 'commercial'
+  map.connect 'orders/:id/survey/:action', :controller => 'survey'
+  map.connect 'orders/:id/graphic_conception/:action', :controller => 'graphic_conception'
+  map.connect 'orders/:id/estimate/:action', :controller => 'estimate'
+  map.connect 'orders/:id/invoicing/:action', :controller => 'invoicing'
 end
