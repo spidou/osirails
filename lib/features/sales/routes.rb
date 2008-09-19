58db391c2123a241e1018455a5a25a2dab001684
Benjamin LAN-SUN-LUK <Benjamin.LAN-SUN-LUK@supinfo.com>
2008-09-19 06:36:47 +0000

    Feature Ventes: routes

--- lib/features/sales/routes.rb
+++ lib/features/sales/routes.rb
@@ -1,7 +1,10 @@
 ActionController::Routing::Routes.add_routes do |map|
   map.connect 'orders/:id/commercial/:action', :controller => 'commercial'
-  map.connect 'orders/:id/survey/:action', :controller => 'survey'
-  map.connect 'orders/:id/graphic_conception/:action', :controller => 'graphic_conception'
-  map.connect 'orders/:id/estimate/:action', :controller => 'estimate'
+  map.connect 'orders/:id/commercial/survey/:action', :controller => 'survey'
+  map.connect 'orders/:id/commercial/graphic_conception/:action', :controller => 'graphic_conception'
+  map.connect 'orders/:id/commercial/estimate/:action', :controller => 'estimate'
   map.connect 'orders/:id/invoicing/:action', :controller => 'invoicing'
+  
+  map.prospectives 'prospective', :controller => 'commercial'
+  map.sales 'sales', :controller => 'invoicing'
 end
