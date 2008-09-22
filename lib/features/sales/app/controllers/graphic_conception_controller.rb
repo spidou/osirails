1bde6f982f39cbdcc616fc4daf26b0f6f58a477d
Benjamin LAN-SUN-LUK <Benjamin.LAN-SUN-LUK@supinfo.com>
2008-09-22 06:35:59 +0000

    Feature Sales

--- lib/features/sales/app/controllers/graphic_conception_controller.rb
+++ lib/features/sales/app/controllers/graphic_conception_controller.rb
@@ -1,3 +1,3 @@
-class GraphicConception < ApplicationController
+class GraphicConceptionsController < ApplicationController
   
 end
--- lib/features/sales/routes.rb
+++ lib/features/sales/routes.rb
@@ -1,12 +1,20 @@
 ActionController::Routing::Routes.add_routes do |map|
   # TODO enlever les map.connect pour les remplacer par des sous ressources d'order
-  map.connect 'orders/new', :controller => 'commercial', :action => 'new'
-  map.connect 'orders/:id/commercial/:action', :controller => 'commercial'
-  map.connect 'orders/:id/commercial/survey/:action', :controller => 'survey'
-  map.connect 'orders/:id/commercial/graphic_conception/:action', :controller => 'graphic_conception'
-  map.connect 'orders/:id/commercial/estimate/:action', :controller => 'estimate'
-  map.connect 'orders/:id/invoicing/:action', :controller => 'invoicing'
+  #map.connect 'orders/new', :controller => 'commercial', :action => 'new'
+  #map.connect 'orders/:id/commercial/:action', :controller => 'commercial'
+  #map.connect 'orders/:id/commercial/survey/:action', :controller => 'survey'
+  #map.connect 'orders/:id/commercial/graphic_conception/:action', :controller => 'graphic_conception'
+  #map.connect 'orders/:id/commercial/estimate/:action', :controller => 'estimate'
+  #map.connect 'orders/:id/invoicing/:action', :controller => 'invoicing'
   
-  map.prospectives 'prospective', :controller => 'commercial'
-  map.sales 'sales', :controller => 'invoicing'
+  map.resources :orders do |order|
+    order.resources :commercial
+    order.resources :survey
+    order.resources :graphic_conception
+    order.resources :estimate
+    order.resources :invoicing
+  end
+  
+  map.prospectives 'prospective', :controller => 'orders', :step => 'commercial'
+  map.sales 'sales', :controller => 'orders', :step => 'invoicing'
 end
