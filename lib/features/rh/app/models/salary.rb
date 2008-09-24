6829ec051e21ec3142b95e94bbd77bf9b8a15bd9
Julien MORILLE <gun974@gmail.com>
2008-09-24 13:04:18 +0000

    modif salaires

--- lib/features/rh/app/models/salary.rb
+++ lib/features/rh/app/models/salary.rb
@@ -1,5 +1,4 @@
 class Salary < ActiveRecord::Base
-
-  
-  belongs_to :job_contracts
+  belongs_to :job_contract
+  validates_format_of :salary , :with => /^[1-9]+(\d)*((\x2E)(\d)*)+$/ , :message => "le montant du salaire doit être un nombre"
 end
