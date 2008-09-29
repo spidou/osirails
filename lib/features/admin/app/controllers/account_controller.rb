b281414f8d6511ed2bda52775c6d0b4747d6eb59
Benjamin LAN-SUN-LUK <Benjamin.LAN-SUN-LUK@supinfo.com>
2008-09-29 10:18:21 +0000

    Bug loggin

--- lib/features/admin/app/controllers/account_controller.rb
+++ lib/features/admin/app/controllers/account_controller.rb
@@ -74,7 +74,10 @@ class AccountController < ApplicationController
   end
   
   def expired_password
-    redirect_to :action => 'logout' if (Time.now - current_user.last_activity) > 5.minutes
+    if (Time.now - current_user.last_activity) > 5.minutes
+      redirect_to :action => 'logout'
+      return
+    end
     flash.now[:error] = "Votre mot de passe est expir&eacute;. Merci d'en choisir un autre."
     return unless request.post?
     user = current_user
