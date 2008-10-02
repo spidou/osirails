83b5a8b4a16e81b2f978a7e10f5820a475ba0765
Benjamin LAN-SUN-LUK <Benjamin.LAN-SUN-LUK@supinfo.com>
2008-10-02 13:40:33 +0000

    Feature Sales: gestion des historiques

--- lib/features/sales/app/models/order_log.rb
+++ lib/features/sales/app/models/order_log.rb
@@ -7,6 +7,7 @@ class OrderLog < ActiveRecord::Base
   serialize :parameters
   
   def self.set(order, user, params)
+    return false if order.nil?
     parameters = params.dup
     self.create :order_id => order.id, :user_id => user.id, :controller => parameters.delete(:controller), :action => parameters.delete(:action), :parameters => parameters
   end
