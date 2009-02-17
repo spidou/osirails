b0be3bddf4c863baa5a420dd28c2edcddd67e93b
Julien MORILLE <gun974@gmail.com>
2009-02-17 05:26:39 +0000

    modification of feature  RH(vues) :job_contract,premia, iban,base ressources

--- lib/features/rh/app/models/premium.rb
+++ lib/features/rh/app/models/premium.rb
@@ -3,4 +3,10 @@ class Premium < ActiveRecord::Base
   
   belongs_to :employee 
   validates_format_of :premium , :with => /^[1-9]+(\d)*((\x2E)(\d)*)+$/ , :message => "le montant de la prime doit être un nombre"
+
+  cattr_accessor :form_labels
+
+  @@form_labels = Hash.new
+  @@form_labels[:premium] = "Prime :"
+  @@form_labels[:remark] = "Motif :" 
 end
