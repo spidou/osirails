3851ee33128f1f2ee3c039e4cf048748eb3b9c68
Julien MORILLE <gun974@gmail.com>
2009-02-26 07:13:08 +0000

    modification of feature  third (customer & suplliers) helpers et views (new edit show index)

--- lib/features/thirds/app/helpers/customers_helper.rb
+++ lib/features/thirds/app/helpers/customers_helper.rb
@@ -1,33 +1,31 @@
 module CustomersHelper
   
   # This method permit to test permission for add button
-  def display_customer_add_button
+  def show_customer_add_button(txt="")
     if controller.can_add?(current_user) and Customer.can_add?(current_user)
-      add_button = []
-      add_button << "<h1><span class='gray_color'>Action</span> <span class='blue_color'>possible</span></h1><ul><li>"
-      add_button << link_to("<img src='/images/add_16x16.png' alt='Ajouter' title='Ajouter' /> Ajouter un client", new_customer_path)
-      return add_button << "</li></ul>"
+      link_to("#{image_tag("/images/add_16x16.png", :alt => "Ajouter", :title => "Ajouter")} #{txt}", new_customer_path)
+
     end
   end
   
   # This method permit to test pemission for view button
-  def display_customer_view_button(customer)
+  def show_customer_view_button(customer,txt="")
     if controller.can_view?(current_user) and Customer.can_view?(current_user)
-      link_to(image_tag("/images/view_16x16.png", :alt =>"D&eacute;tails", :title =>"D&eacute;tails"), customer_path(customer)) 
+      link_to("#{image_tag("/images/view_16x16.png", :alt =>"D&eacute;tails", :title =>"D&eacute;tails")} #{txt}", customer_path(customer)) 
     end
   end
   
   # This method permit to test pemission for edit button
-  def display_customer_edit_button(customer)
+  def show_customer_edit_button(customer,txt="")
     if controller.can_edit?(current_user) and Customer.can_edit?(current_user)
-      link_to(image_tag("/images/edit_16x16.png", :alt =>"Modifier", :title =>"Modifier"), edit_customer_path(customer)) 
+      link_to("#{image_tag("/images/edit_16x16.png", :alt =>"Modifier", :title =>"Modifier")} #{txt}", edit_customer_path(customer)) 
     end
   end
   
   # This method permit to test permission for delete button
-  def display_customer_delete_button(customer)
+  def show_customer_delete_button(customer,txt="")
     if controller.can_delete?(current_user) and Customer.can_delete?(current_user)
-      link_to(image_tag("/images/delete_16x16.png", :alt => "Supprimer", :title => "Supprimer"), customer, {:method => :delete, :confirm => 'Etes vous s&ucirc;r ?'})
+      link_to("#{image_tag("/images/delete_16x16.png", :alt => "Supprimer", :title => "Supprimer")} #{txt}", customer, {:method => :delete, :confirm => 'Etes vous s&ucirc;r ?'})
     end
   end
   
--- lib/features/thirds/app/helpers/suppliers_helper.rb
+++ lib/features/thirds/app/helpers/suppliers_helper.rb
@@ -1,33 +1,30 @@
 module SuppliersHelper
   
   # This method permit to test permission for add button
-  def display_add_button
+  def show_add_button(txt="")
     if controller.can_add?(current_user) and Supplier.can_add?(current_user)
-      add_button = []
-      add_button << "<h1><span class='gray_color'>Action</span> <span class='blue_color'>possible</span></h1><ul><li>"
-      add_button << link_to("<img src='/images/add_16x16.png' alt='Ajouter' title='Ajouter' /> Ajouter un fournisseur", new_supplier_path)
-      return add_button << "</li></ul>"
+      link_to("#{image_tag("/images/add_16x16.png", :alt => "Ajouter", :title => "Ajouter")} #{txt}", new_supplier_path)
     end
   end
   
   # This method permit to test pemrission for view button
-  def display_view_button(supplier)
+  def show_view_button(supplier,txt="")
     if controller.can_view?(current_user) and Supplier.can_view?(current_user)
-      link_to(image_tag("/images/view_16x16.png", :alt =>"D&eacute;tails", :title =>"D&eacute;tails"), supplier_path(supplier)) 
+      link_to("#{image_tag("/images/view_16x16.png", :alt =>"D&eacute;tails", :title =>"D&eacute;tails")} #{txt}", supplier_path(supplier)) 
     end
   end
   
   # This method permit to test pemrission for edit button
-  def display_edit_button(supplier)
+  def show_edit_button(supplier,txt="")
     if controller.can_edit?(current_user) and Supplier.can_edit?(current_user)
-      link_to(image_tag("/images/edit_16x16.png", :alt =>"Modifier", :title =>"Modifier"), edit_supplier_path(supplier)) 
+      link_to("#{image_tag("/images/edit_16x16.png", :alt =>"Modifier", :title =>"Modifier")} #{txt}", edit_supplier_path(supplier)) 
     end
   end
   
   # This method permit to test permission for delete button
-  def display_delete_button(supplier)
+  def show_delete_button(supplier,txt="")
     if controller.can_delete?(current_user) and Supplier.can_delete?(current_user)
-      link_to(image_tag("/images/delete_16x16.png", :alt => "Supprimer", :title => "Supprimer"), supplier, {:method => :delete, :confirm => 'Etes vous s&ucirc;r ?'})
+      link_to("#{image_tag("/images/delete_16x16.png", :alt => "Supprimer", :title => "Supprimer")} #{txt}", supplier, {:method => :delete, :confirm => 'Etes vous s&ucirc;r ?'})
     end
   end
   
