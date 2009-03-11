835779730c97e366eebc24c8f8731f9785aafac1
Benjamin LAN-SUN-LUK <benjamin.lan-sun-luk@supinfo.com>
2009-03-11 22:54:50 +0100

    Improve feature controller

--- lib/features/admin/app/controllers/features_controller.rb
+++ lib/features/admin/app/controllers/features_controller.rb
@@ -1,75 +1,122 @@
 class FeaturesController < ApplicationController
-
-  # Add permissions for methods
-  method_permission(:edit => ['disable', 'enable', 'install', 'uninstall', 'change_state'], :delete => ['remove_feature'], :new => ['add_feature'])
-
-  # Method to collect features and to show them into the index view
+  # GET /features
   def index
     @features = Feature.find(:all, :order => "installed, activated DESC, name")
   end
-  
-  # method to show warning message about uninstallation and to confirm the uninstallation
+
+  # GET /features/1
   def show
     @feature = Feature.find(params[:id])
   end
-  
-  
-  # flash[:notice] shows method's succes message
-  # flash[:error] shows method's failure message with some details concerning dependances  
-  def change_state(method) 
-    @feature = Feature.find(params[:id])   
-    
-    if @feature.send(method)
-      flash[:notice] = @feature.display_flash_notice(method) 
-    else 
-      flash[:error] = @feature.display_flash_error(method)  
-    end
-    redirect_to features_path
-  end
-  
-  # Method for untar an uploaded feature to the default feature path
-  def add_feature
+
+  # POST /features
+  def create
     file_to_upload = {:file => params[:upload]}
-    state = Feature.add(file_to_upload)
-    
-    if state == true
+
+    if Feature.add(file_to_upload)
       flash[:notice] = "Fichier envoy&eacute; et ajout&eacute; avec succ&egrave;s."
-    elsif state == false
-      flash[:error] = "Erreur lors de l'envoi du fichier"
     else
-      flash[:error] = "#{state}"
+      flash[:error] = "Erreur lors de l'envoi du fichier"
     end
+
     redirect_to features_path
   end
-  
-  # Method to show up messages concerning features removing
-  # it call the generic method change state below and pass into arguments his name
-  def remove_feature
-   self.change_state("remove")
+
+  # PUT /features/1
+  def update
+    @feature = Feature.find(params[:id])
+
+    flash[:notice] = "Le module '#{@feature.title}' a &eacute;t&eacute; "
+    case params[:state]
+    when 'enable'
+      if @feature.enable
+        flash[:notice] << "activ&eacute;"
+      else
+        flash[:error] = "Une erreur est survenue lors de l'activation du module '#{self.title}'. "
+        unless @feature.activate_dependencies.empty?
+          @feature.activate_dependencies.size > 1 ? flash[:error] << "Les modules suivants sont requis : " : flash[:error] << "Le module suivant est requis : "
+          deps = []
+          @feature.activate_dependencies.each do |error|
+            deps << "#{error[:title]} " + (error[:version].class == Array ? "v#{error[:version].join(", v:")}" : "v#{error[:version]}")
+          end
+          flash[:error] << deps.join(", ")
+        end
+      end
+    when 'disable'
+      if @feature.disable
+        flash[:notice] << "d&eacute;sactiv&eacute;"
+      else
+        flash[:error] = "Une erreur est survenue lors de la désactivation du module '#{self.title}'. "
+        unless @feature.deactivate_children.empty?
+          @feature.deactivate_children.size > 1 ? flash[:error] << "D'autres modules dépendent de ce module : " : flash[:error]  << "Un autre module dépend de ce module : "
+          deps = []
+          @feature.deactivate_children.each do |error|
+            deps << "#{error[:title]} " + (error[:version].class == Array ? "v#{error[:version].join(", v:")}" : "v#{error[:version]}")
+          end
+          flash[:error] << deps.join(", ")
+        end
+      end
+    when 'install'
+      if @feature.install
+        flash[:notice] << "install&eacute;"
+      else
+        flash[:error] = "Une erreur est survenue lors de l'installaton du module '#{self.title}'. "
+        unless @feature.able_to_install_dependencies.empty?
+          @feature.able_to_install_dependencies.size > 1 ? flash[:error] << "Les modules suivants sont requis : " : flash[:error] <<  "Le module suivants est requis : "
+          deps = []
+          @feature.able_to_install_dependencies.each do |error|
+            deps << "#{error[:name]} " + (error[:version].class == Array ? "v#{error[:version].join(", v:")}" : "v#{error[:version]}")
+          end
+          flash[:error] << deps.join(", ")
+        end
+        unless @feature.able_to_install_conflicts.empty?
+          @feature.able_to_install_conflicts.size > 1 ? flash[:error] << "<br/>Les conflits suivants ont été détectés : " : flash[:error] << "<br />Le conflit suivant a été détecté : "
+          deps = []
+          @feature.able_to_install_conflicts.each do |error|
+            deps << "#{error[:title]} " + (error[:version].class == Array ? "v#{error[:version].join(", v:")}" : "v#{error[:version]}")
+          end
+          flash[:error] << deps.join(", ")
+        end
+        unless @feature.install_log.empty?
+          @feature.install_log.each do |error|
+            flash[:error] << "<br />" + error
+          end
+        end
+      end
+    when 'uninstall'
+      if @feature.uninstall
+        flash[:notice] << "d&eacute;sinstall&eacute;"
+      else
+        flash[:error] = "Une erreur est survenue lors de la désinstallation du module '#{self.title}'. "
+        unless @feature.able_to_uninstall_children.empty?
+          flash[:error] << "D'autres modules dépendent de ce module : "
+          deps = []
+          @feature.able_to_uninstall_children.each do |error|
+            deps << "#{error[:title]} " + (error[:version].class == Array ? "v#{error[:version].join(", v:")}" : "v#{error[:version]}")
+          end
+          flash[:error] << deps.join(", ")
+        end
+        unless @feature.uninstall_log.empty?
+          @feature.uninstall_log.each do |error|
+            flash[:error] << "<br />" + error
+          end
+        end
+      end
+    end
+
+    redirect_to features_path
   end
 
-  # Method to show up messages concerning features enabling
-  # it call the generic method change state below and pass into arguments his name
-  def enable
-    self.change_state("enable")
-  end
+  # DELETE /features/1
+  def delete
+    @feature = Feature.find(params[:id])
 
- # Method to show up messages concerning features disabling
-# it call the generic method change state below and pass into arguments his name
-  def disable
-    self.change_state("disable")
-  end
+    if @feature.remove
+      flash[:notice] = "Le module '#{@feature.title}' a &eacute;t&eacute; supprim&eacute;"
+    else
+      flash[:error] = "Une erreur est survenue lors de la suppression du module '#{@feature.title}'."
+    end
 
-  # Method to show up messages concerning features installing
-# it call the generic method change state below and pass into arguments his name
-  def install
-   self.change_state("install")
+    redirect_to feature_path
   end
-  
-  # Method to show up messages concerning features uninstalling
-# it call the generic method change state below and pass into arguments his name 
-  def uninstall
-   self.change_state("uninstall")
-  end
-  
 end
