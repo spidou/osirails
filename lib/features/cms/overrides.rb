ccf1e24a4a80a74284ceb8a0198ca6e6b1ea9c05
Benjamin LAN-SUN-LUK <benjamin.lan-sun-luk@supinfo.com>
2009-03-18 13:18:51 +0100

    Improve dependency management between menu and content

---
+++ lib/features/cms/overrides.rb
@@ -0,0 +1,12 @@
+require_dependency 'menu'
+
+class Menu
+  has_one :content
+
+  def can_has_this_parent?(new_parent_id)
+    return true if new_parent_id.blank?
+    new_parent = Menu.find(new_parent_id)
+    return false if new_parent.id == self.id or new_parent.ancestors.include?(self) or !new_parent.content.nil?
+    true
+  end
+end
