0572b49ab644ce72d3c22a5cc7eab1852aa5a5f3
Mathieu Fontaine <spidou@gmail.com>
2009-05-28 11:26:12 +0400

    Fix a bug in sales initialization file which stopped rake tasks like 'rake db:create' or 'rake db:reset' when the database is empty (no tables)

--- lib/features/sales/init.rb
+++ lib/features/sales/init.rb
@@ -1,5 +1,5 @@
 require File.join(RAILS_ROOT, 'lib', 'initialize_feature.rb')
-if init(config, directory).feature.activated?
+if conf = init(config, directory) and conf.feature.activated?
   require File.join(directory, '../sales/lib/step_initialize.rb')
   step_initialize(directory)
 end
