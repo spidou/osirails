c8d0d955d168db848e0ecd1ea3427089119af3e7
Mathieu Fontaine <spidou@gmail.com>
2010-01-22 16:34:57 +0400

    Fix a bug when launch test for has_search_index with greater version than current (like rails 2.3.4) of rails in your gems

--- lib/plugins/has_search_index/test/boot.rb
+++ lib/plugins/has_search_index/test/boot.rb
@@ -1,5 +1,5 @@
 plugin_root = File.join(File.dirname(__FILE__), '..')
-version = ENV['RAILS_VERSION']
+version = ENV['RAILS_VERSION'] || "2.1.0"
 version = nil if version and version == ""
 
 # first look for a symlink to a copy of the framework
